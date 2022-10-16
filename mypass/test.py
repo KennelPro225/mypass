08db8af4ee558d3685c27f82457d30f35e3de67f
KennelPro225 <kennelkassi12@gmail.com>
2022-10-16 18:21:24 +0000

    ok

--- mypass/test.py
+++ mypass/test.py
@@ -1,1 +1,15 @@
-'/usr/bin/wkhtmltopdf'
+import pdfkit
+pdfkit.from_url('https://www.google.com/', 'shaurya.pdf')
+import pdfkit
+
+#Define path to wkhtmltopdf.exe
+path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+
+#Define path to HTML file
+path_to_file = 'sample.html'
+
+#Point pdfkit configuration to wkhtmltopdf.exe
+config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+#Convert HTML file to PDF
+pdfkit.from_file(path_to_file, output_path='sample.pdf', configuration=config)
